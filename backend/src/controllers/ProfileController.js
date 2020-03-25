7a3e8b6b0433b40295a519e69712e27fd4eb61fa
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-03-25 15:37:45 -0300

    Profile Controller Corrections

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -2,9 +2,9 @@ const connection = require('../database/connection');
 
 module.exports = {
     async index(req, resp) {
-        const ong_id = request.headers.authorization;
+        const ong_id = req.headers.authorization;
 
-        const incidents = await connection('incidentes')
+        const incidents = await connection('incidents')
             .where('ong_id', ong_id)
             .select('*');
 
